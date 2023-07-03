fd64d7666537f4dcf9e0ffebbf2077d71f11d6e5
Troy Miller <1030781@student.sae.edu.au>
2023-07-03 12:36:30 +1000

    Version 0.6

--- Assets/Scripts/Troy's Stuff/Movement1.cs
+++ Assets/Scripts/Troy's Stuff/Movement1.cs
@@ -11,13 +11,25 @@ public class Movement1 : MonoBehaviour
 
     Vector2 movement;
 
-    
+    public Animator myAnim;
+
+    private void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-      
+
+        if (Input.GetAxisRaw("Horizontal") == 0.1 || Input.GetAxisRaw("Horizontal") == -0.1 || Input.GetAxisRaw("Vertical") == 0.1 || Input.GetAxisRaw("Vertical") == -0.1)
+        {
+            myAnim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        }
+
     }
     private void FixedUpdate()
     {
