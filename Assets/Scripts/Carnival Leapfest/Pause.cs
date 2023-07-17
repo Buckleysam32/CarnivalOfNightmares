07f07395204c5a228a46f98d1b9315712bff0f33
Sam Buckley <1029099@student.sae.edu.au>
2023-07-17 13:24:02 +1000

    content lock commit

--- Assets/Scripts/Carnival Leapfest/Pause.cs
+++ Assets/Scripts/Carnival Leapfest/Pause.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
     bool isPaused;
+    public GameObject PausePrefab;
+    public GameObject menuButton;
 
     // Start is called before the first frame update
     void Start()
     {
-  
+        PausePrefab.SetActive(false);
+        menuButton.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
     }
 
     // Update is called once per frame
@@ -33,10 +39,19 @@ public class Pause : MonoBehaviour
     {
        Time.timeScale = 0;
         isPaused = true;
+        PausePrefab.SetActive(true);
+        menuButton.SetActive(true);
+    }
+
+    public void goToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void ResumeGame()
     {
+        PausePrefab.SetActive(false);
+        menuButton.SetActive(false);
         Time.timeScale = 1;
         isPaused = false;
     }
