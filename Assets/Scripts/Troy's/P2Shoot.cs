02cfb3044632d40ae845704506c66054ed9b694f
Troy Miller <1030781@student.sae.edu.au>
2023-07-31 16:39:13 +1000

    Update 0.5 - Playable edit

--- Assets/Scripts/Troy's/P2Shoot.cs
+++ Assets/Scripts/Troy's/P2Shoot.cs
@@ -14,7 +14,7 @@ public class P2Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightControl))
+        if (Input.GetKey(KeyCode.Keypad0))
         {
             p2isshooting = true;
         }
