454eac71bbd84321cbed0518a5ed9a19bb5afe5b
Troy Miller <1030781@student.sae.edu.au>
2023-08-14 11:30:20 +1000

    Update 1.0.1 - Menus

--- Assets/Scripts/_Base/MainMenu.cs
+++ Assets/Scripts/_Base/MainMenu.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    public void LoadScene(int buildIndex)
+    public void toMenu()
     {
-        GameEvents.LoadScene?.Invoke(buildIndex);
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Game()
+    {
+        SceneManager.LoadScene("GameMenu");
+    }
+    public void Player1()
+    {
+        SceneManager.LoadScene("ShooterP1");
+    }
+    public void Player2()
+    {
+        SceneManager.LoadScene("ShooterP2");
+    }
+    public void Player3()
+    {
+        SceneManager.LoadScene("ShooterP3");
     }
 
     public void Options()
