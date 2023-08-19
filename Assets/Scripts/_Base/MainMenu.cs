acfc2fa46cf5fbf8fce86a7c0a8aeb42d8e874fb
Troy Miller <1030781@student.sae.edu.au>
2023-08-19 10:58:27 +1000

    Update 1.2.2 - Menu Fixed

--- Assets/Scripts/_Base/MainMenu.cs
+++ Assets/Scripts/_Base/MainMenu.cs
@@ -8,22 +8,27 @@ public class MainMenu : MonoBehaviour
     public void toMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        Time.timeScale= 1f;
     }
     public void Game()
     {
         SceneManager.LoadScene("GameMenu");
+        Time.timeScale = 1f;
     }
     public void Player1()
     {
         SceneManager.LoadScene("ShooterP1");
+        Time.timeScale = 1f;
     }
     public void Player2()
     {
         SceneManager.LoadScene("ShooterP2");
+        Time.timeScale = 1f;
     }
     public void Player3()
     {
         SceneManager.LoadScene("ShooterP3");
+        Time.timeScale = 1f;
     }
 
     public void Options()
